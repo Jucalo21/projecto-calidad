e93bcc850b870ed67e8972856be4fbf20ace1e3c
Juan Lopez <jucalo2195@gmail.com>
2022-11-21 11:12:35 -0500

    Commit 22

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    //codigos que usan jquery
+    //codigo para mostrar y ocultar menu de navegacion con el boton
+    configurarInicio();
+    $('.myicon').click(function(e){
+        $('#navbar ul').toggleClass('lista-sm');
+    })
+
+    function configurarInicio(){
+        //codigo para saber en donde se encuentra el usuario
+        var urlPath=window.location.pathname;
+        $('nav a').each(function(){
+            var href=$(this).attr('href');
+            var indice=urlPath.length - href.length;
+            if(urlPath.substring(indice)==href){
+                $(this).closest('li').addClass('active');
+            }
+        })
+    }
+})
