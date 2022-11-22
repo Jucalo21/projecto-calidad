4f1e9ecfa0ccf52406989971ac53af14b3a1623d
LijnD <Luico199@gmail.com>
2022-11-22 01:27:39 -0500

    commit 31

--- js/localInicio.js
+++ js/localInicio.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
-    var txt_nombre = localStorage.getItem('Email');
-    var txt_contraseña = localStorage.getItem('Password');
+    $('#Salidafrom').submit(function () {
+        //obtener los datos de input
+        var txtEmail = $('#correo').val();
+        var txtPassword = $('#contraseña').val();
+        //llama los datos de localStorage
+        var txt_email = localStorage.getItem('Email');
+        var txt_contraseña = localStorage.getItem('Password');
+        //compara los datos de localstorage y input
+        if (txt_email    == txtEmail && txt_contraseña == txtPassword) {
+            //se llama al documento donde va a iniciar
+            window.location = 'ssession.html';
+        } else {
 
-    if (txt_nombre != null && txt_contraseña != undefined) {
-        //se llama al documento donde va a iniciar
-        window.location = 'ssession.html';
-    } else {
-
-    }
+        }
+    });
 });
